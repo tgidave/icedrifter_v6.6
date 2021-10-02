32926e39b468835929932844e76aa37b78ab7317
tgidave <dave@tgi.com>
2021-10-02 11:29:03 -0600

    Turned off serial debut.

--- icedrifter_v6.5/icedrifter.h
+++ icedrifter_v6.5/icedrifter.h
@@ -34,7 +34,7 @@
 
 //To turn off the debugging messages, comment out the next line.
 
-#define SERIAL_DEBUG
+//#define SERIAL_DEBUG
 
 //The following defines are used to control what data is transmitted during debugging.
 //If "SERIAL_DEBUG" is not defined they have no effect.
